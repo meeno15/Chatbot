7d0bd88c2dfcee36a2a5850d04356efc08a16700
Alamin MY <57276152+meeno15@users.noreply.github.com>
2023-05-12 01:13:26 +0400

    Add files via upload

---
+++ bot_training.py
@@ -0,0 +1,123 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, _tree
+
+# Importing the dataset
+training_dataset = pd.read_csv('/Users/alaminmy/Downloads/PP/Training.csv')
+test_dataset = pd.read_csv('/Users/alaminmy/Downloads/PP/Testing.csv')
+
+# Slicing and Dicing the dataset to separate features from predictions
+X = training_dataset.iloc[:, 0:132].values
+y = training_dataset.iloc[:, -1].values
+
+# Dimensionality Reduction for removing redundancies
+dimensionality_reduction = training_dataset.groupby(training_dataset['prognosis']).max()
+
+# Encoding String values to integer constants
+labelencoder = LabelEncoder()
+y = labelencoder.fit_transform(y)
+
+# Splitting the dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+# Implementing the Decision Tree Classifier
+classifier = DecisionTreeClassifier()
+classifier.fit(X_train, y_train)
+
+# Saving the information of columns
+cols = training_dataset.columns
+cols = cols[:-1]
+
+# Checking the Important features
+importances = classifier.feature_importances_
+indices = np.argsort(importances)[::-1]
+features = cols
+
+
+# Implementing the Visual Tree
+def print_disease(node):
+    node = node[0]
+    val = node.nonzero()
+    disease = labelencoder.inverse_transform(val[0])
+    return disease
+
+
+def tree_to_code(tree, feature_names):
+    tree_ = tree.tree_
+    feature_name = [
+        feature_names[i] if i != _tree.TREE_UNDEFINED else "undefined!"
+        for i in tree_.feature
+    ]
+
+    symptoms_present = []
+
+    def recurse(node, depth):
+        nonlocal symptoms_present
+        indent = "  " * depth
+        if tree_.feature[node] != _tree.TREE_UNDEFINED:
+            name = feature_name[node]
+            threshold = tree_.threshold[node]
+            print(f"{name} ?")
+            ans = input().lower()
+            if ans == 'yes':
+                val = 1
+            else:
+                val = 0
+            if val <= threshold:
+                recurse(tree_.children_left[node], depth + 1)
+            else:
+                symptoms_present.append(name)
+                recurse(tree_.children_right[node], depth + 1)
+        else:
+            present_disease = print_disease(tree_.value[node])
+            print(f"You may have {present_disease[0]}")
+            red_cols = dimensionality_reduction.columns
+            symptoms_given = red_cols[dimensionality_reduction.loc[present_disease].values[0].nonzero()]
+            print(f"Symptoms present: {list(symptoms_present)}")
+            print(f"Symptoms given: {list(symptoms_given)}")
+            confidence_level = (1.0 * len(symptoms_present)) / len(symptoms_given)
+            print(f"Confidence level is {confidence_level}")
+            print("The model suggests:")
+            row = doctors[doctors['disease'] == present_disease[0]]
+            print(f"Consult {row['name'].values}")
+            print(f"Visit {row['link'].values}")
+
+    recurse(0, 1)
+
+# This section of code to be run after scraping the data
+
+doc_dataset = pd.read_csv('/Users/alaminmy/Downloads/PP/doctors_dataset.csv', names = ['Name', 'Description'])
+
+
+diseases = dimensionality_reduction.index
+diseases = pd.DataFrame(diseases)
+
+doctors = pd.DataFrame()
+doctors['name'] = np.nan
+doctors['link'] = np.nan
+doctors['disease'] = np.nan
+
+doctors['disease'] = diseases['prognosis']
+
+
+doctors['name'] = doc_dataset['Name']
+doctors['link'] = doc_dataset['Description']
+
+record = doctors[doctors['disease'] == 'AIDS']
+record['name']
+record['link']
+
+
+
+
+
+# Method to simulate the working of a Chatbot by extracting and formulating questions
+def execute_bot():
+    print("Please reply with yes/Yes or no/No for the following symptoms")
+    tree_to_code(classifier, cols)
+
+execute_bot()
